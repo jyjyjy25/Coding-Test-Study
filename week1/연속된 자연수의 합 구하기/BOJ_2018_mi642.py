52db6204f84711e3c742d7aed32b250904d7fb0d
mi642 <68195384+mi642@users.noreply.github.com>
2023-08-20 21:53:34 +0900

    [ADD] BOJ 주몽

--- week1/연속된 자연수의 합 구하기/BOJ_2018_mi642.py
+++ week1/연속된 자연수의 합 구하기/BOJ_2018_mi642.py
@@ -1,4 +1,4 @@
-# BOJ_2018_연속된 자연수
+# BOJ_2018_수들의 합5
 # 난이도: 실버5
 # 풀이 날짜: 23.08.19
 # 풀이 시간:
@@ -8,5 +8,22 @@
 
 # Code
 
+import math
 
-## 메모리:  KB, 시간:  ms
+n = int(input())
+end = 0
+sum = 0
+count = 0
+
+    
+for start in range(0, n):
+    while sum < n and end < n :
+        sum += end + 1
+        end += 1
+    if sum == n:
+        count += 1
+    sum -= start + 1
+
+print(count)
+
+## 메모리: 33376 KB, 시간: 4548 ms
